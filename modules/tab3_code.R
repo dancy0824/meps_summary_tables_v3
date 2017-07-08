02be681d45f1fa15cbd939bbd32413899a2fcb55
emitch8 <mitcheem@gmail.com>
2017-07-08 18:47:13 -0400

    Moving app-specific code builder from module to app_code.R

--- modules/tab3_code.R
+++ modules/tab3_code.R
@@ -1,13 +1,4 @@
 #######################################################
-###                   FUNCTIONS                     ###
-#######################################################
-
-readCode <- function(fileName,path=""){
-  fileLoc = paste0(path,fileName)
-  readChar(fileLoc,file.info(fileLoc)$size)
-}
-
-#######################################################
 ###                      UI                         ###
 #######################################################
 
@@ -30,159 +21,62 @@ codeUI <- function(id){
 ###                     SERVER                      ###
 #######################################################
 
-get_sas_code <- function(subgrps,stat='totEXP'){
-
-# Generic code loading
-  load_code <- readCode("../shared/code/sas/load_fyc.sas")
-
-  real_grps <- subgrps %>% unique
-  if(length(real_grps) > 1) real_grps <- real_grps[real_grps!="ind"]
-
-  ## add agevar if needed
-
-  grp_code <- sapply(real_grps, function(x)
-    readCode(sprintf("../shared/code/sas/grps/%s.sas",x))) %>%
-    paste(collapse="\n")
-
-  stat_code <- readCode(sprintf("code/sas/stats/%s.sas",stat))
-
-# SAS-specific
-
-  format <- paste(real_grps,paste0(real_grps,"."),collapse=" ")
-  domain <- paste(real_grps,collapse="*")
-
-  paste(load_code, grp_code, stat_code, collapse="\n") %>%
-    rsub(type='sas',
-         yy = 14, year = 2014,
-         format = format,
-         domain = domain)
-
-  #%>% writeLines
-}
-
-
-# 
-# get_sas_code(c('ind','sex'),stat="totEXP") %>% writeLines
-# 
-
-
 codeModule <- function(input, output, session, inputs){
-  
-  # r_code <- reactive({
-  #   yr = max(inputs()$years) %>% substring(3,4)
-  # 
-  #   main_code <- fyc_code(stat=inputs()$stat,yr=yr, 
-  #                         subgrps=c(inputs()$rows, inputs()$cols))
-  # 
-  #   tagList(
-  #     tags$p(
-  #       "The following R code can be used to generate the selected estimates.
-  #       For trend estimates, example code is shown for the most recent year selected."),
-  #     
-  #     tags$p(
-  #       "Load R packages and define function to download MEPS data from web:"
-  #     ),
-  #       
-  #     tags$pre(tags$code(snp_load$load_packages)),
-  #     
-  #     tags$p("Run analyses based on selected statistic and grouping variables:"),
-  #     tags$pre(tags$code(main_code))
-  #    
-  #   )
-  # })
-  # 
-  # sas_code <- reactive({
-  #   tagList(
-  #     tags$p("The following SAS code can be used to generate the selected estimates.
-  #       For trend estimates, example code is shown for the most recent year selected."),
-  #     
-  #     tags$pre(tags$code(
-  #       get_sas_code(subgrps = c(inputs()$rows,inputs()$cols))
-  #     ))
-  #   )
-  # })
-  # 
-  # stata_code <- reactive({
-  #   tagList(
-  #     tags$p("You must be an economist. We would love to have Stata code here. 
-  #            If you would like to contribute, send us an email!")
-  #   )
-  # })
-  # 
-  # output$code <- renderUI({
-  #   
-  #   if(input$code_language=="R") return(r_code())
-  #   if(input$code_language=="SAS") return(sas_code())
-  #   if(input$code_language=="Stata") return(stata_code())
-  # })  
-  
-  
-  # output$load_code <- renderText({
-  #   snp_load$load_packages
-  # })
-  # 
-  # output$code <- renderText({
-  #   
-  
-  #    
-  #   # code_string_use(
-  #   #   stat = stat(), 
-  #   #   grp1=inputs()$rows, 
-  #   #   grp2 = inputs()$cols,
-  #   #   year=max(inputs()$years))
-  #   
-  # })
-  # outputOptions(output,"code",suspendWhenHidden = FALSE) 
+   
+  grps <- reactive(c(inputs()$rows, inputs()$cols))
+  year <- reactive(max(inputs()$years))
+  yr <- reactive(substring(year(),3,4))
+  stat <- reactive(inputs()$stat)
+ 
+  r_code <- reactive({
 
+    main_code <- get_r_code(grps=grps(),stat=stat(),yr=yr())
 
-# 
-#   r_code <- reactive({
-#     yr = max(inputs()$years) %>% substring(3,4)
-# 
-#     main_code <- fyc_code(stat=inputs()$stat,yr=yr,
-#                           subgrps=c(inputs()$rows, inputs()$cols))
-# 
-#     tagList(
-#       tags$p(
-#         "The following R code can be used to generate the selected estimates.
-#         For trend estimates, example code is shown for the most recent year selected."),
-# 
-#       tags$p(
-#         "Load R packages and define function to download MEPS data from web:"
-#       ),
-# 
-#       tags$pre(tags$code(snp_load$load_packages)),
-# 
-#       tags$p("Run analyses based on selected statistic and grouping variables:"),
-#       tags$pre(tags$code(main_code))
-# 
-#     )
-#   })
-# 
-#   sas_code <- reactive({
-#     tagList(
-#       tags$p("The following SAS code can be used to generate the selected estimates.
-#         For trend estimates, example code is shown for the most recent year selected."),
-# 
-#       tags$pre(tags$code(
-#         get_sas_code(subgrps = c(inputs()$rows,inputs()$cols))
-#       ))
-#     )
-#   })
-# 
-#   stata_code <- reactive({
-#     tagList(
-#       tags$p("You must be an economist. We would love to have Stata code here.
-#              If you would like to contribute, send us an email!")
-#     )
-#   })
-# 
-  output$code <- renderUI({
-    "hi"
-    # if(input$code_language=="R") return(r_code())
-    # if(input$code_language=="SAS") return(sas_code())
-    # if(input$code_language=="Stata") return(stata_code())
+    tagList(
+      tags$p(
+        "The following R code can be used to generate the selected estimates.
+         For trend estimates, example code is shown for the most recent year selected."),
+      
+      tags$p("Load R packages and define subgroups (if applicable):"),
+      tags$pre(tags$code(load_code(grps(),stat(),year=year(),lang="r"))),
+      
+      tags$p("Run analyses based on selected statistic and grouping variables:"),
+      tags$pre(tags$code(main_code))
+    )
+
   })
+  
+
+  sas_code <- reactive({
+    
+    main_code <- get_sas_code(grps=grps(),stat=stat(),yr=yr())
+    
+    tagList(
+      tags$p("The following SAS code can be used to generate the selected estimates.
+        For trend estimates, example code is shown for the most recent year selected."),
+
+      tags$p("Load data and define subgroups (if applicable):"),
+      tags$pre(tags$code(load_code(grps(),stat(),year=year(),lang="sas"))),
+      
+      tags$p("Run analyses based on selected statistic and grouping variables:"),
+      tags$pre(tags$code(main_code))
+    )
+  })
+  
+  stata_code <- reactive({
+    tagList(
+      tags$p("You must be an economist. We would love to have Stata code here.
+             If you would like to contribute, send us an email!")
+    )
+  })
+
+  output$code <- renderUI({
+
+    if(input$code_language=="R") return(r_code())
+    if(input$code_language=="SAS") return(sas_code())
+    if(input$code_language=="Stata") return(stata_code())
+  })
+  
 
 }
 
