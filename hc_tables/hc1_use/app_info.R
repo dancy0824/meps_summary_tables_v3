5ecf8715117dc452c394e9a789c9f8e65bf28cf5
Emily Mitchell <emily.mitchell@ahrq.hhs.gov>
2017-07-13 12:55:33 -0400

    Editing wording in info box

--- hc_tables/hc1_use/app_info.R
+++ hc_tables/hc1_use/app_info.R
@@ -6,17 +6,28 @@ info <- list(
   
   title = "Use and Expenditures",
 
-  description = 
-    "This interactive table has data on utilization and expenditures of care, including
-      mean and median expenses per person, total number of healthcare events and average cost per event.
-      Data can be grouped by event type (prescription medicines, dental services), source of payment (Medicare,
-      Medicaid, etc.), or demographic characteristics (age, race, sex,...).",
+  description = "The MEPS online summary tables provide an interactive platform allowing users to quickly 
+    access statistics on health care utilization and expenditures. Some of the types of data available here include: mean and 
+    median expenditures per person, total number of healthcare events and average spending per event type. 
+    Data can be grouped by event type (such as prescription medicines or dental services), 
+    by source of payment (such as Medicare or Medicaid), or by demographic characteristics (such as age, race or sex).",
   
-  instructions = "Use the options below to select a
-      variable of interest, as well as whether to view the data across time ('Trends over time') or in a single year ('Cross-sectional data').
-      The 'Customize' dropdown allows additional stratification by grouping variables, and an
-      option to view standard errors. Here are some extra words to make the introduction
-      paragraph a bit longer..."
+  instructions = "Use the options below to select a variable of interest, 
+    the data view ('Trends over time' or 'Cross-sectional'), data years, and grouping variables.
+    If the 'Trends over time' view is selected, a range of years can be chosen.
+    The 'Cross-sectional' view displays a single year, which can be stratified by two grouping variables.
+    Once a grouping variable is selected, a dropdown will appear, enabling selection of specific levels in
+    each group, including population totals.
+
+    After selecting the available options, the data Table will automatically be updated. 
+    The data can be viewed as a plot under the 'Plot' tab, with line graphs for Trends over time, 
+    and grouped bar graphs for the cross-sectional view. The 'Code' tab displays R and SAS code needed to 
+    replicate the data shown in the Table. 
+    The generated Table, Plot, and Codes can be downloaded using the download button under each tab.
+
+    To view standard errors in the Table or 95% confidence intervals in the Plot, 
+    select the 'Show Standard Errors' checkbox."
+
 )
 
 
