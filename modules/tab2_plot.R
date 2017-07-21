80852568c3d867973f5397fb2316723c2fc2d13d
Emily Mitchell <emily.mitchell@ahrq.hhs.gov>
2017-07-21 09:26:01 -0400

    Organizing plot module by splitting line and bar graph

--- modules/tab2_plot.R
+++ modules/tab2_plot.R
@@ -75,6 +75,7 @@ plotUI<- function(id){
 
 plotModule <- function(input, output, session, tbl, inputs, adj, labels){
   
+  rowsX <- reactive(inputs()$rowsX)
   is_trend <- reactive(input$tabs == "trend")
   
   sub_caption <- reactive({
@@ -88,9 +89,8 @@ plotModule <- function(input, output, session, tbl, inputs, adj, labels){
   output$plot_footnote <- renderText(labels()$footnotes$suppress %>% gsub(" -- Estimates","<em>Note:</em> Some estimates",.))
   output$plot_caption  <- renderUI(tags$caption(caption()))
   output$sub_caption <- renderUI(tags$div(class="sub-caption",sub_caption()))
-  
-  rows  <- reactive(inputs()$rows)
-  rowsX <- reactive(inputs()$rowsX)
+
+  ############# Data ############# 
   
   plot_data <- reactive({
     D  <- adj()$D
@@ -98,66 +98,55 @@ plotModule <- function(input, output, session, tbl, inputs, adj, labels){
     
     if(is_trend()) df$x = df$Year
     
-    df <- df %>%
+    df %>%
       mutate(y = coef/D, y_se = se/D) %>%
-      select(grp, x, y, y_se) 
-    
-    if(!is_trend()){
-      df <- df  %>%
-        mutate(
-          x = abbrev(x),
-          x = str_wrap(x,20),
-          x = factor(x, levels = unique(x)))
-    }
-    
-    df #%>%  na.omit
+      select(grp, x, y, y_se)  %>% 
+      mutate(grp = factor(grp, levels = rev(unique(grp))))
   })
+  
+  ############# Formats ############# 
   
   colors <- reactive({
     ncolors = length(unique(plot_data()$grp))
     b_colors <- colorRampPalette(brewer.pal(ncolors,"Dark2"))
     b_colors(ncolors)
   })
-
-  #######################################
-  # GGPLOT
- 
-  ggplot_data <- reactive({
-    out <- plot_data() %>%
-      mutate(grp = factor(grp, levels = rev(unique(grp))))
-    
-    if(!is_trend()){
-      out <- out %>% mutate(x = factor(x, levels = rev(unique(x))))
-    }
-   
-    out
-  })    
   
-  line2 <- reactive({ 
-    p <- ggplot(ggplot_data(),aes(x = x, y = y, fill=grp)) +
+  format_type <- reactive({
+    if(grepl('percent',caption())) return(percent)
+    if(grepl('expend',caption())) return(dollar)
+    return(comma)
+  })
+
+  
+  ############# Plots ############# 
+ 
+  line <- reactive({ 
+    p <- ggplot(plot_data(),aes(x = x, y = y, fill=grp)) +
       scale_fill_manual(values=colors())
 
     if(inputs()$showSEs){
-      p <- p +
-        geom_ribbon(aes(ymin = y-1.96*y_se, ymax = y+1.96*y_se),alpha=0.3) 
+      p <- p + geom_ribbon(aes(ymin = y-1.96*y_se, ymax = y+1.96*y_se),alpha=0.3) 
     }
     
     p + geom_line(aes(col=grp),size = 1) +
       geom_point(aes(col=grp),size = 2) +
-      scale_color_manual(values=colors())
-    
+      scale_color_manual(values=colors()) + 
+      expand_limits(y=0)
   })
   
-  bar2 <- reactive({  
+  bar <- reactive({  
+    dat <- plot_data() %>%
+      mutate(
+        one = 1,
+        x = abbrev(x),
+        x = str_wrap(x,20),
+        x = factor(x, levels = unique(x)),
+        x = factor(x, levels = rev(unique(x))))
     
-    dat <- ggplot_data()
-    dat$one = 1
-    
-    gp <- ggplot(dat,aes(x = x, y = y, fill=grp)) +
-      scale_fill_manual(values=colors(),drop=FALSE)
-    
-    p <- gp +
-      geom_bar(stat = "identity", position = "dodge", colour="white") #+ 
+    p <- ggplot(dat,aes(x = x, y = y, fill=grp)) +
+      scale_fill_manual(values=colors(),drop=FALSE) +
+      geom_bar(stat = "identity", position = "dodge", colour="white") + 
       scale_x_discrete(drop=FALSE) 
     
     if(inputs()$showSEs){
@@ -166,7 +155,6 @@ plotModule <- function(input, output, session, tbl, inputs, adj, labels){
                       width = 0, position = position_dodge(width = 0.9)) + 
         geom_line(aes(x=one,y=one,color="95% Confidence Interval"))+
         scale_color_manual(values=c("95% Confidence Interval" = 'black'))
-
     }
     
     p + coord_flip() +
@@ -174,26 +162,17 @@ plotModule <- function(input, output, session, tbl, inputs, adj, labels){
              color = guide_legend(order=2))
   })
 
-  format_type <- reactive({
-    if(grepl('percent',caption())) return(percent)
-    if(grepl('expend',caption())) return(dollar)
-    return(comma)
-  })
+  
+  ############# Dispay (PLOTLY) ############# 
   
   gv <- function(){
     xlabel = grp_labels[[rowsX()]]
     
-    if(is_trend()) gp <- line2() else gp <- bar2()
-    gp <- gp + theme_minimal(base_size=16) +
-        xlab(xlabel) + 
-        ylab("") +
+    if(is_trend()) gp <- line() else gp <- bar()
+    
+    gp + theme_minimal(base_size=16) + xlab(xlabel) + ylab("") +
       theme(legend.title=element_blank()) +
       scale_y_continuous(labels = format_type()) 
-    
-    if(is_trend()){
-      gp <- gp + expand_limits(y=0)
-    }
-    gp
   }
   
   output$plot <- renderPlotly({
@@ -216,17 +195,15 @@ plotModule <- function(input, output, session, tbl, inputs, adj, labels){
     )
   })
   
-  ####################################
+  ############# Download (GGPLOT) ############# 
   
   outgg <- function(){
-    #gv() + ggtitle(str_wrap(caption(),60))
     gv() + labs(title = str_wrap(caption(),60),
                 subtitle = str_wrap(sub_caption(),60))
   }
   
   output$png <- downloadHandler(
     filename = function(){ paste('meps-hc-',Sys.Date(),'.png',sep='') },
-    
     content = function(file) {
       ggsave(file,plot=outgg(),device = "png",width=10,height=6.25)
     })  
