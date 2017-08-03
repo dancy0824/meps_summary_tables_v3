4fda7f31aaf5c359810c4f66f96ad73bd926f500
Emily Mitchell <emily.mitchell@ahrq.hhs.gov>
2017-08-03 12:10:41 -0400

    Updating description for care app

--- hc_tables/hc2_care/app_info.R
+++ hc_tables/hc2_care/app_info.R
@@ -5,18 +5,29 @@
 info <- list(
   
   title = "Accessibility and quality of care",
+ 
+  description = "This MEPS online summary table provides statistics on accessibility and quality of care,
+  such as the percent of the population with a usual source of care, difficulty accessing needed care,
+  persons with diabetes care, and patient-reported quality of doctor's visits. Data can be viewed over 
+  time or for a single year by demographic characteristics (such as age, race or sex).",
   
-  description = 
-    "This interactive table has data on access to and quality of care, including 
-    data on medical care associated with diabetes, preventive health care services, 
-    barriers to health care services, access to health care services, 
-    and experiences during delivery of health care services",
+  instructions = "Use the options below to select a variable of interest, 
+  the data view ('Trends over time' or 'Cross-sectional'), data years, and grouping variables.
+  If the 'Trends over time' view is selected, a range of years can be chosen.
+  The 'Cross-sectional' view displays a single year, which can be stratified by two grouping variables.
+  Once a grouping variable is selected, a dropdown will appear, enabling selection of specific levels in
+  each group, including population totals.
   
-  instructions = 
-   "Use the following options to select a variable of interest, as well as 
-    whether to view the data across time ('Trends over time') or in a single year ('Cross-sectional data').
-    The 'Customize' dropdown allows additional stratification by grouping varaibles, and an
-    option to view standard errors"
+  After selecting the available options, the data Table will automatically be updated. 
+  The data can be viewed as a plot under the 'Plot' tab, with line graphs for Trends over time, 
+  and grouped bar graphs for the cross-sectional view. The 'Code' tab displays R and SAS code needed to 
+  replicate the data shown in the Table. 
+  The generated Table, Plot, and Codes can be downloaded using the download button under each tab.
+  
+  To view standard errors in the Table or 95% confidence intervals in the Plot, 
+  select the 'Show Standard Errors' checkbox."
   
 )
+
+
 
