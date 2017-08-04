463da6b31318451edd36bae3b9bfa934e806d0c7
Emily Mitchell <emily.mitchell@ahrq.hhs.gov>
2017-08-04 09:13:44 -0400

    Adding / fixing download code button for R/SAS code:

--- modules/tab3_code.R
+++ modules/tab3_code.R
@@ -7,18 +7,23 @@ codeUI <- function(id){
   
   tabPanel(title="Code",icon=icon("code"),
            
-           downloadButton508( ns("dl_code"), 
-                              class = 'download-button',
-                              label = 'Download Code', 
-                              icon=icon('download')),
+           div(class='code-select',
+             selectInput508(ns("code_language"),
+                            label = "Select Programming Language:",
+                            #choices = c("R","SAS","Stata")),
+                            choices = c("R","SAS")),
+             
+             downloadButton508( ns("dl_code"), 
+                                class = 'download-button',
+                                label = 'Download Code', 
+                                icon=icon('download'))
+            ),
            
+           tags$p(
+             "The following code can be used to generate the selected estimates.
+              For trend estimates, example code is shown for the most recent year selected:"),
            
-           selectInput508(ns("code_language"),
-                          label = "Select Programming Language:",
-                          #choices = c("R","SAS","Stata")),
-                          choices = c("R","SAS")),
-           
-           uiOutput(ns("code"),role="region","aria-live"="polite")
+           tags$pre(tags$code(uiOutput(ns("code"),role="region","aria-live"="polite")))
       )
   
 }
@@ -34,40 +39,17 @@ codeModule <- function(input, output, session, inputs){
   year <- reactive(max(inputs()$years))
   yr <- reactive(substring(year(),3,4))
   stat <- reactive(inputs()$stat)
+  
+  lang <- reactive(input$code_language)
  
   r_code <- reactive({
-
     main_code <- get_r_code(grps=grps(),stat=stat(),yr=yr())
-
-    tagList(
-      tags$p(
-        "The following R code can be used to generate the selected estimates.
-         For trend estimates, example code is shown for the most recent year selected."),
-      
-      tags$p("Load R packages and define subgroups (if applicable):"),
-      tags$pre(tags$code(load_code(grps(),stat(),year=year(),lang="r"))),
-      
-      tags$p("Run analyses based on selected statistic and grouping variables:"),
-      tags$pre(tags$code(main_code))
-    )
-
+    paste(load_code(grps(),stat(),year=year(),lang="r"),main_code)
   })
   
-
   sas_code <- reactive({
-    
     main_code <- get_sas_code(grps=grps(),stat=stat(),yr=yr())
-    
-    tagList(
-      tags$p("The following SAS code can be used to generate the selected estimates.
-        For trend estimates, example code is shown for the most recent year selected."),
-
-      tags$p("Load data and define subgroups (if applicable):"),
-      tags$pre(tags$code(load_code(grps(),stat(),year=year(),lang="sas"))),
-      
-      tags$p("Run analyses based on selected statistic and grouping variables:"),
-      tags$pre(tags$code(main_code))
-    )
+    paste(load_code(grps(),stat(),year=year(),lang="sas"),main_code)
   })
   
   stata_code <- reactive({
@@ -77,13 +59,21 @@ codeModule <- function(input, output, session, inputs){
     )
   })
 
-  output$code <- renderUI({
-
-    if(input$code_language=="R") return(r_code())
-    if(input$code_language=="SAS") return(sas_code())
-    if(input$code_language=="Stata") return(stata_code())
+  out_code <- reactive({
+    if(lang()=="R") return(r_code())
+    if(lang()=="SAS") return(sas_code())
+    if(lang()=="Stata") return(stata_code())
   })
   
+  output$code <- renderUI(out_code())
+
+  ## Download code
+  
+  output$dl_code <- downloadHandler(
+    filename = function(){ sprintf('meps-%s-code.%s',lang(),lang()) },
+    content = function(file){
+       write(out_code(),file=file) 
+    })   
 
 }
 
