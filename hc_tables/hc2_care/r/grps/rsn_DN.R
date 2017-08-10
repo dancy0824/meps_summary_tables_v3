d025c1e7a11d1f8e25b4e1d96010ef57cc7efe15
Emily Mitchell <emily.mitchell@ahrq.hhs.gov>
2017-08-10 14:07:19 +0000

    Changing capitalization

---
+++ hc_tables/hc2_care/r/grps/rsn_DN.R
@@ -0,0 +1,5 @@
+FYC <- FYC %>%
+  mutate(delay_DN  = (DNUNAB42 == 1|DNDLAY42==1)*1,
+         afford_DN = (DNDLRS42==1|DNUNRS42==1)*1,
+         insure_DN = (DNDLRS42 %in% c(2,3)|DNUNRS42 %in% c(2,3))*1,
+         other_DN  = (DNDLRS42 > 3|DNUNRS42 > 3)*1)
