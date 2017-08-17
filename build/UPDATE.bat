5577d0957531f8eb65b23e36e788d6d89f7241de
Emily Mitchell <emily.mitchell@ahrq.hhs.gov>
2017-08-17 14:21:37 -0400

    Adding 2015 data

--- build/UPDATE.bat
+++ build/UPDATE.bat
@@ -1,7 +1,7 @@
 PATH=C:\Users\emily.mitchell\Documents\R\R-3.3.3\bin
 
-Rscript shared\puf_update.R
-Rscript shared\puf_transfer.R
+Rscript puf_update.R
+Rscript puf_transfer.R
 
 cd "hc1_use"
 START update_USE.bat
